2790fe7c8cc0be77186268f115b69641dd32f5a4
anyerin01 <anyerin01@gmail.com>
2021-03-15 12:26:07 -0400

    clean code

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -14,9 +14,20 @@ fi
 hostname=$(hostname -f)
 lscpu_out=`lscpu`
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU\sMHz" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2\scache:" | awk '{print $3}' | xargs)
 
+function print_() {
+    echo -e "$1: $2\n"
+}
 
-#DO NOT put extra empty lines, it's ugly
+print_ cpu_number "$cpu_number"
+print_ cpu_architecture "$cpu_architecture"
+print_ cpu_model "$cpu_model"
+print_ cpu_mhz "$cpu_mhz"
+print_ l2_cache "$l2_cache"
 
 #put appropriate exit number
 exit 0
