ca85efd083a717926f6645d028f77596a41b6000
anyerin01 <anyerin01@gmail.com>
2021-03-17 15:00:55 -0400

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -8,7 +8,9 @@ psql_user=$4 #postgres
 psql_password=$5 #password
 command=$6
 
-list=('connect' 'run')
+export PGPASSWORD=$psql_password
+
+list=('set' 'run')
 
 ##validate arguments
 if [ "$#" -ne 6 ]; then
@@ -16,39 +18,34 @@ if [ "$#" -ne 6 ]; then
     exit 1
 fi
 
-function set_up() {
-  hostname=$(hostname -f)
-  lscpu_out=`lscpu`
-  cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-  cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
-  cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
-  cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU\sMHz" | awk '{print $3}' | xargs)
-  l2_cache=$(echo "$lscpu_out"  | egrep "^L2\scache:" | awk '{print $3}' | xargs)
-  memory_info=$(cat /proc/meminfo)
-  total_mem=$(echo "$memory_info"  | egrep "^MemTotal:" | awk '{print $2}' | xargs)
-  timestamp=$(echo "$(date '+%Y-%m-%d %H:%M:%S' -u)" | awk '{print $1"\t"$2}' | xargs)
+hostname=$(hostname -f)
+lscpu_out=`lscpu`
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU\sMHz" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+memory_info=$(cat /proc/meminfo)
+total_mem=$(echo "$memory_info"  | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(echo "$(date '+%Y-%m-%d %H:%M:%S' -u)" | awk '{print $1"\t"$2}' | xargs)
 
-  insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, time_)
-  VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', $total_mem, '$timestamp');"
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, time_)
+VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', $total_mem, '$timestamp');"
 
-  check_hostname="SELECT EXISTS (SELECT id FROM host_info WHERE hostname='$hostname')::int;"
-}
+check_hostname="SELECT EXISTS (SELECT id FROM host_info WHERE hostname='$hostname')::int;"
+
 
 function connect_psql() {
-  export PGPASSWORD=$psql_password
   psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -f ../sql/ddl.sql
 }
 
 function run_psql() {
-  set_up
-  export PGPASSWORD=$psql_password
-  psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$1"
+  psql -t -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$1"
 }
 
 function is_exist() {
-  run_psql "$check_hostname"
-  check=$?
-  if [ $check = 0 ]
+  get=$(run_psql "$check_hostname")
+  if [ "$get" == "      0" ]
     then run_psql "$insert_stmt"
     else
       echo "HOST EXISTS"
@@ -92,3 +89,12 @@ exit 0
 #VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, '$l2_cache', $total_mem, '$timestamp');"
 #
 #check_hostname="SELECT EXISTS (SELECT id FROM host_info WHERE hostname='$hostname')::int;"
+
+#insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, time_)
+#VALUES ('host9', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300.000, 256, 751324, '2021-03-17 16:58:58');"
+
+#insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, time_)
+#VALUES ('$hostname', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300.000, 256, 751324, '2021-03-17 16:58:58');"
+#check_hostname="SELECT EXISTS (SELECT id FROM host_info WHERE hostname='$hostname')::int;"
+#check_hostname="SELECT EXISTS (SELECT id FROM host_info WHERE hostname='$hostname')::int;"
+#SELECT EXISTS (SELECT 1 FROM host_info WHERE hostname='hostname07')::int
