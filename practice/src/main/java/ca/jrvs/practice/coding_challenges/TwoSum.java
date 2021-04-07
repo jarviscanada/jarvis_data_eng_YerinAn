164f395802098ead23b516980d8bd51e337363e1
anyerin01 <anyerin01@gmail.com>
2021-04-07 17:43:13 -0400

    twoSum

---
+++ practice/src/main/java/ca/jrvs/practice/coding_challenges/TwoSum.java
@@ -0,0 +1,69 @@
+package ca.jrvs.practice.coding_challenges;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+public class TwoSum {
+//  public static void main(String[] args) {
+//    int[] nums = { 3,9,5,7,4,1};
+//    int target = 9;
+//    System.out.println(Arrays.toString(twoSumBruteForce(nums, target)));
+//  }
+
+  /**
+   * Time complexity: O(n^2)
+   * nested for loop
+   */
+  public int[] twoSumBruteForce(int[] nums, int target){
+    for(int i=0; i < nums.length; i++){
+      for(int j=1; j<nums.length; j++){
+        if((nums[i] + nums[j] == target) && (nums[i] != nums[j])){
+          return new int[]{nums[i], nums[j]};
+        }
+      }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
+  /**
+   * Time complexity: O(n + nlogn)
+   * Iterate the loop - O(n)
+   * Sorting method - n log n
+   */
+  public int[] twoSumSorting(int[] nums, int target){
+    Arrays.sort(nums);
+    int indexL=0;
+    int indexR=nums.length-1;
+    while(indexR > indexL){
+      int result = nums[indexL] + nums[indexR];
+      if(result > target)
+        indexR--;
+      else if(result < target)
+        indexL++;
+      else{
+        int[] result_arr = new int[]{nums[indexL], nums[indexR]};
+        return result_arr;
+      }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
+  /**
+   * Time complexity: O(n)
+   */
+  public int[] twoSumLinear(int[] nums, int target){
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i = 0; i < nums.length; i++) {
+      if(map.containsKey(target - nums[i]))
+        return new int[]{target - nums[i], nums[i]};
+      map.put(nums[i], target - nums[i]);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/coding_challenges/TwoSumTest.java
@@ -0,0 +1,53 @@
+package ca.jrvs.practice.coding_challenges;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwoSumTest {
+
+  @Test
+  public void twoSumBruteForce() {
+    TwoSum twoSum = new TwoSum();
+    int[] test01 = { 3,9,5,7,4,1};
+    int target = 9;
+    int[] result01 = twoSum.twoSumBruteForce(test01, target);
+    assertArrayEquals(new int[]{5,4}, result01);
+    int[] test02 = {3,9,5,7};
+    try{
+      twoSum.twoSumBruteForce(test02, target);
+    }catch (IllegalArgumentException e){
+      assertTrue(true);
+    }
+  }
+
+  @Test
+  public void twoSumSorting() {
+    TwoSum twoSum = new TwoSum();
+    int[] test01 = { 3,9,5,7,4,1};
+    int target = 9;
+    int[] result01 = twoSum.twoSumSorting(test01, target);
+    assertArrayEquals(new int[]{4,5}, result01);
+    int[] test02 = {3,9,5,7};
+    try{
+      twoSum.twoSumSorting(test02, target);
+    }catch (IllegalArgumentException e){
+      assertTrue(true);
+    }
+  }
+
+  @Test
+  public void twoSumLinear() {
+    TwoSum twoSum = new TwoSum();
+    int[] test01 = { 3,9,5,7,4,1};
+    int target = 9;
+    int[] result01 = twoSum.twoSumLinear(test01, target);
+    assertArrayEquals(new int[]{5,4}, result01);
+    int[] test02 = {3,9,5,7};
+    try{
+      twoSum.twoSumLinear(test02, target);
+    }catch (IllegalArgumentException e){
+      assertTrue(true);
+    }
+  }
+}
