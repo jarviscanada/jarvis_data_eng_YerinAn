b0cc29be2acd3f5a137e5ffbceefe05cc92e2cf8
anyerin01 <anyerin01@gmail.com>
2021-05-03 16:38:44 -0400

    update coding ch

---
+++ practice/src/main/java/ca/jrvs/practice/coding_challenges/MissingNumber.java
@@ -0,0 +1,71 @@
+package ca.jrvs.practice.coding_challenges;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://www.notion.so/jarvisdev/Missing-Number-6f379be9991f4f82b0226dc7ca02b591
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ * Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+ */
+public class MissingNumber {
+
+  public static void main(String[] args) {
+    int[] nums = new int[]{9,6,4,2,3,5,7,0,1};
+    System.out.print(missingNumberGauss(nums));
+  }
+
+  /**
+   * Big-O: O(n) time : (2n)
+   */
+  public static int missingNumberSum(int[] nums) {
+    int sum = 0;
+    int expected = 0;
+    for(int n : nums)
+      sum += n;
+    for(int i = 0; i <= nums.length; i++)
+      expected += i;
+    return expected-sum;
+  }
+
+  /**
+   * Big-O: O(n) time
+   * O(1) space
+   */
+  public static int missingNumberGauss(int[] nums){
+    int sum = 0;
+    int expected = nums.length * (nums.length + 1) / 2;
+    for(int n : nums)
+      sum += n;
+    return expected-sum;
+  }
+
+  /**
+   * Big-O: O(n log n) time
+   */
+  public static int missingNumberSort(int[] nums) {
+    Arrays.sort(nums);
+    for(int i=0; i< nums.length; i++){
+      if(nums[i] != i)
+        return i;
+    }
+    return nums.length;
+  }
+
+  /**
+   * Big-O: O(n) time
+   * O(n) space
+   */
+  public static int missingNumberSet(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for(int n : nums) set.add(n);
+    for(int i = 0; i <= nums.length; i++){
+      if(!set.contains(i))
+        return i;
+    }
+    return -1;
+  }
+
+}
---
+++ practice/src/main/java/ca/jrvs/practice/coding_challenges/RemoveElement.java
@@ -0,0 +1,30 @@
+package ca.jrvs.practice.coding_challenges;
+
+/**
+ * https://www.notion.so/jarvisdev/Remove-Element-c46a16cd128e4455bc10d7da7a762103
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ */
+public class RemoveElement {
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {0, 1, 2, 2, 3, 0, 4, 2};
+    System.out.print(removeElement(nums, 2));
+  }
+
+  /**
+   * Big-O: O(n) time
+   * O(1) space
+   */
+  public static int removeElement(int[] nums, int val) {
+    int size = 0;
+    for(int i = 0; i < nums.length; i++){
+      if(nums[i] != val){
+        nums[size] = nums[i];
+        size++;
+      }
+    }
+    return size;
+  }
+}
